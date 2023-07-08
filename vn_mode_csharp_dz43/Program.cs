5c083cedb9ed8da933e2d8ef5e4ed8ed1121a2c1
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-08 14:00:53 +0300

    ijunior_csharp_dz43

--- vn_mode_csharp_dz43/Program.cs
+++ vn_mode_csharp_dz43/Program.cs
@@ -4,25 +4,25 @@ using System.Linq;
 
 public class Program
 {
-    public const int SHOW_SELLER_ITEMS = 1;
-    public const int SHOW_PLAYER_ITEMS = 2;
-    public const int BUY_ITEM = 3;
-    public const int EXIT = 4;
+    public const int ShowSellerItems = 1;
+    public const int ShowPlayerItems = 2;
+    public const int BuyItem = 3;
+    public const int Exit = 4;
 
     public static void Main()
     {
         Player player = new Player(1000);
         Seller seller = new Seller(500);
 
-        Item item1 = new Item("Книга");
-        Item item2 = new Item("Мяч");
+        Item item1 = new Item("Книга", 150);
+        Item item2 = new Item("Мяч", 50);
 
         seller.AddItem(item1);
         seller.AddItem(item2);
 
         while (true)
         {
-            Console.Write($"1. Товары продавца 2. Ваши товары 3. Купить товар 4. Выход: ");
+            Console.Write(Messages.Choices);
 
             bool isNumber = int.TryParse(Console.ReadLine(), out int choice);
 
@@ -34,30 +34,22 @@ public class Program
 
             switch (choice)
             {
-                case SHOW_SELLER_ITEMS:
+                case ShowSellerItems:
                     seller.ShowItems();
                     break;
 
-                case SHOW_PLAYER_ITEMS:
+                case ShowPlayerItems:
                     player.ShowItems();
                     break;
 
-                case BUY_ITEM:
+                case BuyItem:
                     Console.Write(Messages.EnterItemName);
                     string itemName = Console.ReadLine();
-                    Console.Write(Messages.EnterItemPrice);
-                    bool isDecimal = decimal.TryParse(Console.ReadLine(), out decimal price);
-
-                    if (!isDecimal)
-                    {
-                        Console.WriteLine(Messages.InvalidPriceInput);
-                        break;
-                    }
                     Item itemToBuy = seller.GetItemByName(itemName);
 
                     if (itemToBuy != null)
                     {
-                        player.BuyItem(seller, itemToBuy, price);
+                        player.BuyItem(seller, itemToBuy);
                     }
                     else
                     {
@@ -65,106 +57,17 @@ public class Program
                     }
                     break;
 
-                case EXIT:
+                case Exit:
                     return;
 
                 default:
                     Console.WriteLine(Messages.InvalidChoice);
                     break;
             }
+
+            Console.WriteLine($"У продавца на балансе: {seller.MoneyAmount}");
+            Console.WriteLine($"У вас на балансе: {player.MoneyAmount}");
         }
-    }
-}
-
-public abstract class Trader
-{
-    protected List<Item> _items;
-    protected decimal _money;
-
-    public Trader()
-    {
-        _items = new List<Item>();
-    }
-
-    public void ShowItems()
-    {
-        Console.Write(Messages.ShowItems);
-
-        if (_items.Count == 0)
-        {
-            Console.WriteLine(Messages.NoItems);
-        }
-        else
-        {
-            Console.WriteLine(string.Join(", ", _items.Select(i => i.Name)));
-        }
-    }
-
-    public void AddItem(Item item)
-    {
-        _items.Add(item);
-        Console.WriteLine(Messages.ItemAdded + item.Name);
-    }
-}
-
-public class Player : Trader
-{
-    public void BuyItem(Seller seller, Item item, decimal price)
-    {
-        if (_money < price)
-        {
-            Console.WriteLine(Messages.InsufficientMoney);
-            return;
-        }
-
-        if (seller.SellItem(this, item))
-        {
-            _money -= price;
-        }
-    }
-
-    public Player(decimal money)
-    {
-        _money = money;
-    }
-}
-
-public class Seller : Trader
-{
-    public bool SellItem(Player player, Item item)
-    {
-        if (_items.Contains(item))
-        {
-            _items.Remove(item);
-            player.AddItem(item);
-            Console.WriteLine(Messages.ItemSold + item.Name);
-            return true;
-        }
-        else
-        {
-            Console.WriteLine(Messages.SellerDoesNotHaveItem + item.Name);
-            return false;
-        }
-    }
-
-    public Seller(decimal money)
-    {
-        _money = money;
-    }
-
-    public Item GetItemByName(string itemName)
-    {
-        return _items.FirstOrDefault(i => i.Name.ToLower() == itemName.ToLower());
-    }
-}
-
-public class Item
-{
-    public string Name { get; set; }
-
-    public Item(string name)
-    {
-        Name = name;
     }
 }
 
@@ -178,7 +81,102 @@ public class Messages
     public const string SellerDoesNotHaveItem = "Продавец не имеет такого товара: ";
     public const string InvalidChoice = "Неверный выбор. Введите число от 1 до 4.";
     public const string EnterItemName = "Введите название товара: ";
-    public const string EnterItemPrice = "Введите цену товара: ";
     public const string InvalidPriceInput = "Неверный ввод. Введите числовое значение для цены.";
     public const string SellerDoesNotHaveItemInput = "Товара \"{0}\" нет у продавца.";
+    public const string Choices = "1. Товары продавца 2. Ваши товары 3. Купить товар 4. Выход: ";
+}
+
+public abstract class Trader
+{
+    protected List<Item> Items;
+    protected decimal Money;
+
+    public decimal MoneyAmount => Money;
+
+    public Trader(decimal money)
+    {
+        Items = new List<Item>();
+        Money = money;
+    }
+
+    public void ShowItems()
+    {
+        Console.Write(Messages.ShowItems);
+
+        if (Items.Count == 0)
+        {
+            Console.WriteLine(Messages.NoItems);
+        }
+        else
+        {
+            foreach (Item item in Items)
+            {
+                Console.WriteLine($"{item.Name}, стоимость: {item.Price}");
+            }
+        }
+    }
+
+    public void AddItem(Item item)
+    {
+        Items.Add(item);
+        Console.WriteLine(Messages.ItemAdded + item.Name);
+    }
+}
+
+public class Player : Trader
+{
+    public void BuyItem(Seller seller, Item item)
+    {
+        if (Money < item.Price)
+        {
+            Console.WriteLine(Messages.InsufficientMoney);
+            return;
+        }
+
+        if (seller.SellItem(this, item))
+        {
+            Money -= item.Price;
+        }
+    }
+
+    public Player(decimal money) : base(money) { }
+}
+
+public class Seller : Trader
+{
+    public bool SellItem(Player player, Item item)
+    {
+        if (Items.Contains(item))
+        {
+            Items.Remove(item);
+            player.AddItem(item);
+            Console.WriteLine(Messages.ItemSold + item.Name);
+            Money += item.Price;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine(Messages.SellerDoesNotHaveItem + item.Name);
+            return false;
+        }
+    }
+
+    public Seller(decimal money) : base(money) { }
+
+    public Item GetItemByName(string itemName)
+    {
+        return Items.FirstOrDefault(item => item.Name.ToLower() == itemName.ToLower());
+    }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public Item(string name, decimal price)
+    {
+        Name = name;
+        Price = price;
+    }
 }
